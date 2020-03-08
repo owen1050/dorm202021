3c79e4c8c056701f191d617dbeaaa986f4e6b05f
owen1050 <owen1050@gmail.com>
2020-03-08 19:17:43 -0400

    Owen: Added windows file test

---
+++ Planning/FileWritingTestWindows.py
@@ -0,0 +1,8 @@
+import pathlib
+
+currentPath = str(pathlib.Path().absolute())
+pathAbove = currentPath[0:(currentPath.rfind("\\")+1)]
+logsPath = pathAbove+"Logs\\Logs.txt"
+f = open(logsPath, "r")
+print(f.read())
+f.close()
