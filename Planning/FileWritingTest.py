e7445df7d8dc4a41edc786a62998e6b162263ce1
owen1050 <owen1050@gmail.com>
2020-03-08 19:09:12 -0400

    Owen: Updates to reading logs. WOrks on windows now linux test

--- Planning/FileWritingTest.py
+++ Planning/FileWritingTest.py
@@ -1,3 +1,8 @@
 import pathlib
 
-print(pathlib.Path().absolute())
+currentPath = str(pathlib.Path().absolute())
+pathAbove = currentPath[0:(currentPath.rfind("\\")+1)]
+logsPath = pathAbove+"Logs\\Logs.txt"
+f = open(logsPath, "r")
+print(f.read())
+f.close()
